9a3ed8e30e5a81f65fd4e537f599e04415623891
Allison Richardel <airichardel@gmail.com>
2021-11-04 18:54:50 -0600

    almost done

---
+++ Develop/public/db.js
@@ -0,0 +1,61 @@
+let db;
+let budgetVersion;
+
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+    const { oldVersion } = e;
+    const newVersion = e.newVersion || db.version;
+    db = e.target.result;
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
+
+request.onerror = function (e) {
+    console.log(`Oh no! ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'Post',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    transaction = db.transaction(['BudgetStore'], 'readwrite');
+                    const currentStore = transaction.objectStore('BudgetStore');
+                    currentStore.clear();
+                }
+            });
+        }
+    };
+}
+
+request.onsuccess = function (e) {
+    console.log('success');
+    db = e.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const store = transaction.objectStore('BudgetStore');
+    store.add(record);
+};
+
+window.addEventListener('online', checkDatabase);
